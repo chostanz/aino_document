19c053d16145ecd407754a3e4f8f5e2f01baf19e
chostanz <dewianyar21@gmail.com>
2023-11-03 09:15:46 +0700

    punten

--- models/user_models.go
+++ models/user_models.go
@@ -1,7 +1,7 @@
 package models
 
 type Users struct {
-	User_uuid         int    `json:"user_uuid" db:"user_uuid"`
+	User_uuid         string `json:"user_uuid" db:"user_uuid"`
 	User_name         string `json:"user_name" db:"user_name"`
 	Role_title        string `json:"role_title" db:"role_title"`
 	Application_title string `json:"application_title" db:"application_title"`
