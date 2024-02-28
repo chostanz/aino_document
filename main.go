fbef18c0159a9eb576744e98b7b37482629a2a65
chostanz <dewianyar21@gmail.com>
2024-02-28 12:33:38 +0700

    y

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"aino_document/utils"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
@@ -14,10 +13,10 @@ func main() {
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.CORS())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:4200", "http://localhost:8080"}, // Izinkan akses dari dua origin
-		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},       // Izinkan metode permintaan tertentu
-	}))
+	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	// 	AllowOrigins: []string{"http://localhost:4200", "http://localhost:8080"}, // Izinkan akses dari dua origin
+	// 	AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},       // Izinkan metode permintaan tertentu
+	// }))
 	customValidator := &utils.CustomValidator{Validator: validator.New()}
 	e.Validator = customValidator
 	e.Logger.Fatal(e.Start(":8080"))
